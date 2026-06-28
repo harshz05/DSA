6b232248f75ff5cbde91ab3f686fb8a9f87d9d4d
Harsh Gupta <harsh.guptaa205@gmail.com>
2026-06-28 11:16:37 +0530

    y

---
+++ Problems/1846 - Maximum Element after Decreasing and Rearranging.cpp
@@ -0,0 +1,33 @@
+/*
+=========================================
+LeetCode 1846 - Maximum Element after Decreasing and Rearranging
+=========================================
+
+Difficulty : medium
+
+Pattern    :sorting , greedy
+
+Time        :
+
+Space       :
+
+Date Solved : 2026-06-28
+
+=========================================
+*/
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort( arr.begin() , arr.end()) ;
+      
+        arr[0]  =1 ;
+        for ( int i = 1;i < arr.size() ;i++) {
+            arr[i]=min ( arr[i] , arr[i-1] + 1) ;
+        }
+        return arr.back() ;
+
+
+         
+    }
+};
